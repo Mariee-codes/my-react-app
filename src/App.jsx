de0b319c7f6041f19206d10f11e96a02cff1d769
Mariee-codes <marion.19745@students.kyu.ac.ke>
2026-01-26 15:53:07 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import react from "react"
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Header />
+    </div>
+  )
+}
