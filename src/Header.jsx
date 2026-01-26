96a34089e26249a820d9fc786570ebb24a496fd0
Mariee-codes <marion.19745@students.kyu.ac.ke>
2026-01-26 16:30:19 +0300

    edit in header and index files

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
+import chefClaudeLogo from "./images/Chef Claude.png"
 export default function Header () {
     return (
         <header>
-        <img src ="/images/Chef Claude.png"/>
+        <img src ={chefClaudeLogo}/>
         <h1>Chef Claude</h1>
         </header>
     )
