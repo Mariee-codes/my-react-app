c1bbd204709affe885fac26436a688a46364e513
Mariee-codes <marion.19745@students.kyu.ac.ke>
2026-01-30 16:14:02 +0300

    changes in main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import react from "react";
 import Header from "./Header";
 import './index.css'
+import Main from "./Main"
 
 export default function App() {
   return (
     <div>
         <Header />
+        <Main />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,24 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+export default function Main() {
+  return <h1>Main component</h1>
+}
+
+<main>
+  <form>
+    <input  
+
+    type = "text"
+    placeholder="e.g oregano"
+    aria-label="Add ingredient
+    "/>
+
+    <button>
+      Add ingredient
+    </button>
+    
+
+  </form>
+</main>
